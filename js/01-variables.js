eb4dd576a9e7d72acd8a584b67033ace13f7e429
Javier Jimenez <javier.sniguel@gmail.com>
2023-10-23 10:21:08 -0300

    agregue comentarios de variables de js y creamos un mensaje por consola para mostrar el anio

--- js/01-variables.js
+++ js/01-variables.js
@@ -1,1 +1,9 @@
 /*declarar una variable- tipos de variables: String, number(Decimal y enteros), boolean, null*/
+/*declarar variables con let y const (Ya no se usa var) */
+/*Usamos let cuando sabemos que el dato puede cambiar su valor con el tiempo*/
+/*Usamos const para valores que solo tienen un unico valor y no cambia*/
+let yearCurrent = 2023;
+
+/*mostrar mensaje*/
+
+console.log("año actual: " + yearCurrent);
