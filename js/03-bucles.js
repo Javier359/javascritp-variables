aeb263d87b6c670a25d060e98b61735b5f897857
Javier Jimenez <javier.sniguel@gmail.com>
2023-10-25 11:14:41 -0300

    agregamos un ejemplo de bucle while y do while

---
+++ js/03-bucles.js
@@ -0,0 +1,12 @@
+//aumenta
+let numero =1;
+while(numero <= 10){
+    document.write(`<p>Región número ${numero}</p>`);
+    numero = numero +1;
+}
+//decrementa
+let contador = 10;
+do{
+    document.write(`<p>Región número ${contador} con do-while</p>`);
+    contador--
+}while( contador >= 1)
