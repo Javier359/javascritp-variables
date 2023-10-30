59b32e36a571a32fe25ffca412a2adb5939d190f
Javier Jimenez <javier.sniguel@gmail.com>
2023-10-30 11:17:41 -0300

    Se agrego el ejercicio 10 del tp 2

---
+++ js/tp1-ejercicios/tp2-ejercicio1.js
@@ -0,0 +1,15 @@
+//crear tablas o matrices con bucles anidados
+//````
+const filas = parseInt(prompt("Ingrese las filas"));
+const columnas = parseInt(prompt("Ingrese las columnas"));
+let totalCeldas = filas * columnas;
+document.write(`<table><tbody>`);
+for (let i = 0; i < filas; i++) {
+  document.write(`<tr>`);
+  for(let j =0 ; j<columnas ; j++){
+    document.write(`<td>${totalCeldas}</td>`);
+    totalCeldas--;
+  }
+  document.write(`</tr>`);
+}
+document.write(`</tbody></table>`);
